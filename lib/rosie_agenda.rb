9ad1010df1b844420267db168c657d2362e7ec28
Paul Morganthall <paul@morganthall.com>
2014-11-20 01:29:45 -0500

    Document the reason for JSON parsing.
    The page.content could be directly matched via the unique
    chevrons, but the content is ASCII-8 and the regex is UTF-8
    (Ruby default now). I jiggled both sides and never found a simpler
    way of getting the encoding to match. So I’m leaving the 
    JSON parse.

--- lib/rosie_agenda.rb
+++ lib/rosie_agenda.rb
@@ -35,6 +35,8 @@ module RosieAgenda
 
     def member_id(name)
       page = @fth.get URL, action: "membersearch", term: name
+      # This parse could be avoided (since we're matching on unique chevrons)
+      # but #parse produces a UTF-8 string which matches the UTF-8 regex.
       response = JSON.parse(page.content)[0]
       id = response["value"].match(/«(\d*)»/)[1]
       id || (raise "Unable to find member id in /#{response['value']}/")
