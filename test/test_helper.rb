bf955dd56fdee896c710af7316979d53b0e9ab06
Paul Morganthall <pcm@morganthall.com>
2016-05-16 12:34:49 -0400

    Document VCR password filtering.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,3 +7,13 @@ VCR.configure do |config|
   config.cassette_library_dir = 'test/cassettes'
   config.hook_into :webmock
 end
+
+__END__
+VCR.configure do |c|
+  c.filter_sensitive_data("<SOMESITE_PASSWORD>") do
+    ENV['SOMESITE_PASSWORD']
+    # or $credentials['somesite']['password'] or whatever
+  end
+end
+
+Now, for ENV['SOMESITE_PASSWORD'], it can be real credential by using Figaro gem.
